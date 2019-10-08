e8bdded8cee7cfb7aa1ed04eb26de1b7fcf4eb09
Weaverzhu <weaver_zhu@outlook.com>
2019-10-08 11:26:32 +0800

    fix ex2

--- lab02/SUM_TEN/USER/ex2.s
+++ lab02/SUM_TEN/USER/ex2.s
@@ -1,23 +1,22 @@
-	AREA Reset,DATA,READONLY;创建数据段Reset
+	AREA Reset,DATA,READONLY;?????Reset
 	DCD 0X12345678
 	DCD   Reset_Handler
-	AREA CODE_SEGMET,CODE,READONLY;创建数据段CODE_SEGMENT
+	AREA CODE_SEGMET,CODE,READONLY;?????CODE_SEGMENT
 	ENTRY
 Reset_Handler  proc
 	export Reset_Handler    [weak]
 	MOV R1,#0
 	MOV R2,#1
 	MOV R0,#0
-	ADD R0,R0,R1
-	ADD R0,R0,R2
-	MOV R4,#8;
-start
+	MOV R3,#0
 	ADD R3,R1,R2
+start
 	ADD R0,R0,R3
+	ADD R3,R1,R2
 	MOV R1,R2
 	MOV R2,R3
-	SUBS R4,R4,#1
-	BNE start
+	SUBS R4,R3,#10
+	BLE start
     NOP
 	ENDP
 	END
